8c91b7a8e75396d71aac132bab3fa4d3d39aca7a
shouta2004 <11010312shouta@gmail.com>
2024-11-09 17:50:22 +0900

    弾を発射

--- Assets/19_Takano/bullet.cs
+++ Assets/19_Takano/bullet.cs
@@ -7,11 +7,13 @@ public class bullet : MonoBehaviour
     // Start is called before the first frame update
     public float fMoveSpeed = 7.0f;
     public GameObject BulletObj;
+    public float speed=5.0f;
     Vector2 moveVec2 = new Vector2(1.0f, 0.0f);
-
-    GameObject testObj;
+    Vector2 playerMoveVec;
+    Rigidbody2D player_rb;
     Rigidbody2D testRb;
-    int a = 0;
+    GameObject testObj;
+
 
     [SerializeField] private float time = 0;
 
@@ -20,27 +22,27 @@ public class bullet : MonoBehaviour
         testObj = Instantiate(BulletObj);
         testRb = testObj.GetComponent<Rigidbody2D>();
         InvokeRepeating("RepeatMethod", 1.0f, 1.0f);
+        player_rb=GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         time += Time.deltaTime;
+        playerMoveVec = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            a = 1;
-            GameObject cloneObj;
-           cloneObj= Instantiate(BulletObj);
+         
+            //a = 1;
+            
+            GameObject cloneObj;//ÉNÉçÅ[ÉìÇÃïœêî
+           cloneObj= Instantiate(BulletObj, transform.position, Quaternion.identity);//ï°êª
             Rigidbody2D rb;
-            //cloneObj.transform.Translate(moveVec2.x,Time.deltaTime,0,0);
-            rb= cloneObj.GetComponent<Rigidbody2D>();
-            rb.AddForce(moveVec2, ForceMode2D.Force);
-        }
-
-        if (Input.GetKey(KeyCode.Return))
-        {
-            testRb.AddForce(moveVec2, ForceMode2D.Force);
+            rb= cloneObj.GetComponent<Rigidbody2D>();//ç¿ïWÇéÊìæÇ∑ÇÈ
+            rb.AddForce(moveVec2*fMoveSpeed, ForceMode2D.Impulse);//óÕÇâ¡Ç¶ÇÈ
         }
 
 
@@ -50,6 +52,9 @@ public class bullet : MonoBehaviour
             
             testRb.AddForce(moveVec2 * 10, ForceMode2D.Force);
         }
+
+        Debug.Log(playerMoveVec);
+        player_rb.velocity = playerMoveVec*speed;
     }
     void RepeatMethod()
     {
